a668977fd386973687270f8f9cec4b7621983887
nameERRORka <nameERRORka@yahoo.com>
2020-05-30 10:41:18 +0300

    Создание своей рарности предметов

--- main/java/ru/fcorecode/arcanereborn/configs/Rarity.java
+++ main/java/ru/fcorecode/arcanereborn/configs/Rarity.java
@@ -1,5 +1,26 @@
 package ru.fcorecode.arcanereborn.configs;
 
-public class Rarity {
+import net.minecraft.util.EnumChatFormatting;
 
+public enum Rarity {
+	
+	_useless(EnumChatFormatting.DARK_GRAY, "Предмет"),
+	_common(EnumChatFormatting.GRAY, "Запечатанный"),
+    _uncommon(EnumChatFormatting.WHITE, "Обынчый"),
+    _rare(EnumChatFormatting.BLUE, "Редкий"),
+    _epic(EnumChatFormatting.LIGHT_PURPLE, "Эпический"),
+	_legendary(EnumChatFormatting.GOLD, "Легендарный"),
+	_awakened(EnumChatFormatting.GOLD, "Пробужденный");
+	
+    public final EnumChatFormatting rarityColor;
+    
+    public final String rarityName;
+
+    private static final String __OBFID = "CL_00000056";
+
+    private Rarity(EnumChatFormatting p_i45349_3_, String p_i45349_4_)
+    {
+        this.rarityColor = p_i45349_3_;
+        this.rarityName = p_i45349_4_;
+    }
 }
