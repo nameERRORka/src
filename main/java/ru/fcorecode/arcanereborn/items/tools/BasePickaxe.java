0c71aac000c3d22cd3d13f07ca6e0aaee5c428c5
nameERRORka <nameERRORka@yahoo.com>
2020-06-09 19:55:08 +0300

    Удалил ексампл, изменил ру ланг и баз кирку сделал

--- main/java/ru/fcorecode/arcanereborn/items/tools/BasePickaxe.java
+++ main/java/ru/fcorecode/arcanereborn/items/tools/BasePickaxe.java
@@ -36,8 +36,10 @@ import ru.fcorecode.arcanereborn.configs.ModToolMaterial;
 import net.minecraft.util.StatCollector;
 
 public class BasePickaxe extends ItemPickaxe {
-    public int mode = 4;
-    public String namemode = "none";
+    public int mode = 0;
+
+    public String namemode = "1 на 1";
+
     public BasePickaxe(String name, String texture, int maxStackSize, ToolMaterial baseHAMMER) {
         super(baseHAMMER);
         this.canRepair = false;
@@ -57,114 +59,103 @@ public class BasePickaxe extends ItemPickaxe {
         par3List.add(" " + " ");
         par3List.add(StatCollector.translateToLocal("item.effency.lore") + " " + ModToolMaterial.digSpeedbaseHAMMER);
         par3List.add(" " + " ");
-		par3List.add(StatCollector.translateToLocal("item.par3MEDIUM1.lore"));
-		par3List.add(StatCollector.translateToLocal("item.par3MEDIUM2.lore"));
-		par3List.add(StatCollector.translateToLocal("item.par3MEDIUM3.lore"));
-		par3List.add(StatCollector.translateToLocal("item.par3MEDIUM4.lore"));
-		par3List.add(StatCollector.translateToLocal("item.par3MEDIUM5.lore"));
-	    par3List.add(" " + " ");
-		par3List.add(StatCollector.translateToLocal("item.par3mode.lore")+ " " + namemode);
+        par3List.add(StatCollector.translateToLocal("item.par3MEDIUM1.lore"));
+        par3List.add(StatCollector.translateToLocal("item.par3MEDIUM2.lore"));
+        par3List.add(StatCollector.translateToLocal("item.par3MEDIUM3.lore"));
+        par3List.add(StatCollector.translateToLocal("item.par3MEDIUM4.lore"));
+        par3List.add(StatCollector.translateToLocal("item.par3MEDIUM5.lore"));
+        par3List.add(" " + " ");
+        par3List.add(StatCollector.translateToLocal("item.par3mode.lore") + " " + namemode);
         int a, b, c;
         a = this.getMaxDamage();
         b = this.getDamage(par1ItemStack);
         c = a - b;
-
         par3List.add(" " + " ");
         par3List.add(StatCollector.translateToLocal("item.GetDamage.lore") + " " + c + " " + StatCollector.translateToLocal("item.GetDamageL.lore"));
-
-
     }
-
 
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
         if (world.isRemote) {
-            if (mode == 1) {
-            	
-                player.addChatMessage(new ChatComponentText("Ночное видение"));
-            	mode = 2;
-            	namemode = "NightVision";
-            } else if (mode == 2) { 
-                player.addChatMessage(new ChatComponentText("Увеличен урон"));
-                mode = 3;
-                namemode = "DamageBoost";
-            } else if (mode == 3) {
-                player.addChatMessage(new ChatComponentText("Все было выключено"));
-                mode = 4;
-                namemode = "none";
-            } else if (mode == 4) {
-                player.addChatMessage(new ChatComponentText("Утомление"));
+            if (mode == 0) {
+                player.addChatMessage(new ChatComponentText("Включен режим 3 на 3"));
                 mode = 1;
-                namemode = "digSlowDown";
+                namemode = "3 на 3";
+            } else if (mode == 1) {
+                player.addChatMessage(new ChatComponentText("Включен режим 1 на 1"));
+                mode = 0;
+                namemode = "1 на 1";
             }
         }
-        
+
         itemStack.damageItem(1, player);
         return itemStack;
     }
-    public void onUpdate (ItemStack stack, World world, Entity entity, int par4, boolean par5)
-    {
+    public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5) {
         super.onUpdate(stack, world, entity, par4, par5);
-        if (entity instanceof EntityPlayer)
-        {
+        if (entity instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) entity;
             ItemStack equipped = player.getCurrentEquippedItem();
-            if (equipped == stack && mode == 1){
+            if (equipped == stack && mode == 1) {
                 player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 2, 2, true));
             } else if (equipped == stack && mode == 2) {
                 player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 2, 2, true));
             } else if (equipped == stack && mode == 3) {
                 player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 2, 2, true));
             } else if (equipped == stack && mode == 0) {
-            	player.removePotionEffect(Potion.damageBoost.id);
+                player.removePotionEffect(Potion.damageBoost.id);
             }
-        
 
-}}
+
+        }
+    }
     @Override
     public boolean onBlockStartBreak(ItemStack stack, int x, int y, int z, EntityPlayer player) {
+    	
         World world = player.worldObj;
         Block block = world.getBlock(x, y, z);
+        
+        if (mode == 1) {
+            MovingObjectPosition object = RandomUtils.raytraceFromEntity(world, player, false, 4.5D);
+            if (object == null) {
+                return super.onBlockDestroyed(stack, world, block, x, y, z, player);
+            }
 
-        MovingObjectPosition object = RandomUtils.raytraceFromEntity(world, player, false, 4.5D);
+            int side = object.sideHit;
+            int xmove = 0;
+            int ymove = 0;
+            int zmove = 0;
 
-        if (object == null) {
-            return super.onBlockDestroyed(stack, world, block, x, y, z, player);
-        }
-
-        int side = object.sideHit;
-        int xmove = 0;
-        int ymove = 0;
-        int zmove = 0;
-
-        if (side == 0 || side == 1) {
-            xmove = 1;
-            zmove = 1;
-        } else {
-            ymove = 1;
-            if (side == 4 || side == 5) {
+            if (side == 0 || side == 1) {
+                xmove = 1;
                 zmove = 1;
             } else {
-                xmove = 1;
+                ymove = 1;
+                if (side == 4 || side == 5) {
+                    zmove = 1;
+                } else {
+                    xmove = 1;
+                }
             }
-        }
 
-        float strength = ForgeHooks.blockStrength(block, player, world, x, y, z);
+            float strength = ForgeHooks.blockStrength(block, player, world, x, y, z);
 
-        if (player.isSneaking() && ConfigInfo.EnableHammerShiftOneBlock &&
-            (player.experienceLevel >= 20 || player.capabilities.isCreativeMode)) {
-            checkBlockBreak(world, player, x, y, z, stack, strength, block, side);
-        } else {
-            for (int i = -xmove; i <= xmove; i++) {
-                for (int j = -ymove; j <= ymove; j++) {
-                    for (int k = -zmove; k <= zmove; k++) {
-                        if (i != x && j != y && k != z) {
-                            checkBlockBreak(world, player, x + i, y + j, z + k, stack, strength, block, side);
+            if (player.isSneaking() && ConfigInfo.EnableHammerShiftOneBlock &&
+                (player.experienceLevel >= 20 || player.capabilities.isCreativeMode)) {
+                checkBlockBreak(world, player, x, y, z, stack, strength, block, side);
+            } else {
+                for (int i = -xmove; i <= xmove; i++) {
+                    for (int j = -ymove; j <= ymove; j++) {
+                        for (int k = -zmove; k <= zmove; k++) {
+                            if (i != x && j != y && k != z) {
+                                checkBlockBreak(world, player, x + i, y + j, z + k, stack, strength, block, side);
+                            }
                         }
                     }
                 }
             }
         }
         return false;
+
     }
 
     public void checkBlockBreak(World world, EntityPlayer player, int x, int y, int z, ItemStack stack, float strength, Block originalBlock, int side) {
