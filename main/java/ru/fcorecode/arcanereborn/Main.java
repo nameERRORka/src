0331a894bd2dce0b03dba5029263b29329fb4198
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-01 16:39:02 +0800

    Some final changes

--- main/java/ru/fcorecode/arcanereborn/Main.java
+++ main/java/ru/fcorecode/arcanereborn/Main.java
@@ -22,13 +22,6 @@ public class Main {
 //    public static Item _daemonicBody;
 //    public static Item _daemonicLegs;
 //    public static Item _daemonicSteps;
-//    public static Item _daemonicShatter;
-
-//    public static Item _BaseSkana;
-//    public static Item _IceSkana;
-//    public static Item _BaseBow;
-//    public static Item _LeatherClaws;
-//    public static Potion _potionFly;
 
     @Instance
     public static Main instance = new Main();
@@ -43,31 +36,6 @@ public class Main {
 //        _daemonicBody = new _daemonicArmor(0, 1).setUnlocalizedName("DaemonicBody").setTextureName(MODID + ":armor/_daemonicBody");
 //        _daemonicLegs = new _daemonicArmor(0, 2).setUnlocalizedName("DaemonicLegs").setTextureName(MODID + ":armor/_daemonicLegs");
 //        _daemonicSteps = new _daemonicArmor(0, 3).setUnlocalizedName("DaemonicSteps").setTextureName(MODID + ":armor/_daemonicSteps");
-//        _daemonicShatter = new AFRItems("DaemonicShatter", "items/_daemonicShatter", 64);
-
-       // _mediumHammer = new MediumHammer("MediumHammer", "tools/_hammerzero", 1, ModToolMaterial.MEDIUMHAMMER);
-//        _BaseSkana = new BaseSkana("BaseSkana", "weapon/_baseSkana", 1, ModToolMaterial.SKANA);
-//        _IceSkana = new BaseSkana("IceSkana", "weapon/_IceSword", 1, ModToolMaterial.ICESCANA);
-//        _LeatherClaws = new LeatherClaws("LeatherClaws", "weapon/_LClaws", 1, ModToolMaterial.LClaws);
-//        _MagicRock = new AFRBlocks("MagicRock", Material.rock, 3, 15, "pickaxe", 4, "MagicRock/_MagicRock");
-//        _MagicWood = new AFRBlocks("MagicWood", Material.wood, 3, 15, "axe", 4, "_MagicWood");
-
-//        GameRegistry.registerItem(_daemonicSteps, "DaemonicSteps");
-//        GameRegistry.registerItem(_daemonicLegs, "DaemonicLegs");
-//        GameRegistry.registerItem(_daemonicBody, "DaemonicBody");
-//        GameRegistry.registerItem(_daemonicHelmet, "DaemonicHelmet");
-//        GameRegistry.registerItem(_daemonicShatter, "DaemonicShatter");
-
-//        GameRegistry.registerItem(_BaseSkana, "BaseSkana");
-//        GameRegistry.registerItem(_IceSkana, "IceSkana");
-//        GameRegistry.registerItem(_LeatherClaws, "LeatherClaws");
-//        GameRegistry.registerBlock(_MagicRock, "MagicRock");
-//        GameRegistry.registerBlock(_MagicWood, "MagicWood");
-    	
-//    	ItemStack enchStack = new ItemStack(Main._basepickaxe);
-//    	ItemStack enchStack1 = new ItemStack(net.minecraft.init.Items.diamond_pickaxe);
-//    	enchStack.addEnchantment(EnchantmentRegistry.SAFE_ALL, 1);
-//    	enchStack1.addEnchantment(EnchantmentRegistry.SAFE_ALL, 1);
     }
 
     @EventHandler
